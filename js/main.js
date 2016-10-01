7796a868e168486e89bdd8ebc4e40b1c79048a9a
Gibran Gonzalez <haibrayn@hotmail.com>
2016-10-01 13:30:07 -0500

    Added main js file
    This file will have the logic of the application.

---
+++ js/main.js
@@ -0,0 +1,13 @@
+/* This file will have all the mecanics of the app
+ *
+ * Programa que recibe una gramática regular y produce un autómata finito equivalente y viceversa.
+ *
+ *
+ */
+
+
+// All the code should run in this function
+$(document).ready(function(){
+    /* Code Starts Here */
+    console.log('hello world!');
+});
